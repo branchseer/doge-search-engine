9fefb4cdfa65517e42bb080c9d8fd11da2957937
patr0nus <patronum@outlook.com>
2014-06-01 15:38:07 +0800

    Lexical analysis

--- indexer/lex.py
+++ indexer/lex.py
@@ -1,7 +1,20 @@
-import sys
+from bs4 import BeautifulSoup
+import re
 
-def 
+def tokenize_text(text):
+  return re.findall(re.compile('\w+'), text)
+  
+def tokenize_html(html):
+  soup = BeautifulSoup(html)
+  words = tokenize_text(soup.get_text().lower())
+  links = list()
+  
+  for link in soup.find_all('a'):
+    links.append((link.text, link.get('href')))
+    
+  return words, links
 
 if __name__ == '__main__':
-  from docs import documents
-  
+  words, links = tokenize_html(open('../pages/2013-spring/index.html').read());
+  for text, url in links:
+    print(text)
