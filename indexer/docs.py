718eb2b5ba1d0e60526387d2a82b0aaaf1b1d4d4
patr0nus <patronum@outlook.com>
2014-06-01 15:37:47 +0800

    Document Provider

--- indexer/docs.py
+++ indexer/docs.py
@@ -1,9 +1,7 @@
-import sys
 from os import path, walk
 
-page_folder = path.join('..', 'pages')
 
-def documents():
+def documents(page_folder = path.join('..', 'pages')):
   for dirpath, dirnames, filenames in walk(page_folder):
     for filename in filenames:
       extension = path.splitext(filename)[1]
