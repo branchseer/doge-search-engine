41b0c9f0eb28ab227a37a79f6332bf15baf1452e
patr0nus <patronum@outlook.com>
2014-06-06 20:51:05 +0800

    Add crawler agent

--- crawler/index.py
+++ crawler/index.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
-def crawl(url, depth=0, urlfilter=lambda url: True, maxDepth=float("infinity"), linkset=set()):
+def crawl(url, depth=0, urlfilter=lambda url: True, maxDepth=float("infinity"), linkset=set(), session=requests.Session()):
   content = None
   try:
     content = requests.get(url).text
@@ -26,7 +26,9 @@ if __name__ == '__main__':
       return url.startswith('http://www.seu.edu.cn/s')
     
     base_path = 'pages'
-    for url, content, links in crawl('http://www.seu.edu.cn/s/132/main.jspy', urlfilter=filter):
+    crawler_session = requests.Session()
+    crawler_session.headers['User-Agent'] = 'doge-crawler'
+    for url, content, links in crawl('http://www.seu.edu.cn/s/132/main.jspy', urlfilter=filter, session=crawler_session):
       print(url)
       filepath = path.join(base_path, url[url.find('//') + 2:])
       filedir = path.dirname(filepath)
