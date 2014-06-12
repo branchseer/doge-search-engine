ffb933ff827adc691570dff2b0b54b9e6d1dd35f
patr0nus <patronum@outlook.com>
2014-06-12 22:08:56 +0800

    Muiltiple Keywords

--- app/search.py
+++ app/search.py
@@ -4,18 +4,30 @@ sys.path.append('..')
 from db.sqlite import Session
 from db.models import Term, Document, TermFrequency
 from indexer.stemming import stem
+from indexer.lex import tokenize_text
 
 
 def search(query):
   session = Session()
   try:
-    query = stem(query)
-    term = session.query(Term).filter(Term.text == query).first()
-    if not term:
+    keywords = tokenize_text(query)
+    terms = ((keyword, session.query(Term).filter(Term.text == stem(keyword)).first()) for keyword in keywords)
+    terms = [term for term in terms if term[1] is not None]
+    
+    
+    if len(terms) == 0:
       yield from ()
     else:
-      for freq in term.frequencies:
-        yield (freq.document, freq.frequency)
+      documents = {}
+      for keyword, term in terms:
+        for freq in term.frequencies:
+          termTuple = (keyword, freq.frequency)
+          if freq.document.url not in documents:
+            documents[freq.document.url] = (freq.document, [termTuple])
+          else:
+            documents[freq.document.url][1].append(termTuple)
+      yield from sorted(documents.values(), key=lambda doc: len(doc[1]), reverse=True)
+  
   finally:
     session.flush()
     session.commit()
